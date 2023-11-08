55dfae8279c160427ce6b310650afcf1df4c5053
ntbtrung <trungntb.bi11-275@st.usth.edu.vn>
2023-11-09 01:15:19 +0700

    new file: file upload with progress bar.html 	new file: script.js 	modified: style.css 	new file: upload.php

---
+++ GP_MUSIC_WEB/upload.php
@@ -0,0 +1,5 @@
+<?php
+$file_name = $_FILES['file']['name']; //getting file name
+$tmp_name = $_FILES['file']['tmp_name']; //getting file temporary name
+$file_up_name = time().$file_name; //making file name dynamic by adding time before file name
+move_uploaded_file($tmp_name, "files/".$file_up_name); //moving file to the folder with dynamic name
