01a07b4e5b6d824d71400437a96b1386080e6f6a
ba11-102 <vinhdt.ba11-102@st.usth.edu.vn>
2023-12-15 15:51:35 +0700

    fix bug from label of form input. connect to db. Try to insert data by method post but not done yet

---
+++ GP_MUSIC_WEB/front/register.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost"; // Địa chỉ máy chủ MySQL
+$username = "root"; // Tên người dùng MySQL
+$password = ""; // Mật khẩu MySQL
+$dbname = "social"; // Tên cơ sở dữ liệu MySQL
+
+// Kết nối đến cơ sở dữ liệu
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Fail to connect to sever: " . $conn->connect_error);
+}
+echo '<script>alert("Connect to sever complete")</script>';
+
+
+if (isset($_POST['btn-register'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO `user_test`(`username`,`email`,`password`)
+    VALUES ('$username','$email', md5('$password'))";
+
+    if ($conn->query($sql) === TRUE) {
+        echo '<script>alert("Insert data complete")</script>';
+    } else {
+        echo "Error{$sql}" . $conn->error;
+    }
+}
+
+?>
