96d210c0c161bae540cc553aa0a68789a8934399
Trung274 <trunksadboi@gmail.com>
2023-12-27 10:21:30 +0700

    remove the files regarding to image upload by php, trying new method(nodejs and multer)

--- GP_MUSIC_WEB/database/table.sql
+++ GP_MUSIC_WEB/database/table.sql
@@ -72,7 +72,6 @@ CREATE TABLE `user` (
   `password` varchar(50) NOT NULL,
   `nickname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `phone` varchar(50) DEFAULT NULL,
   `avatar` varchar(200) NOT NULL,
   `biography` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- GP_MUSIC_WEB/front/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server_name = 'localhost';
-$user_name = 'root';
-$password = '';
-$db_name = 'social';
-
-$conn = mysqli_connect($server_name, $user_name, $password, $db_name) or die("could not connect");
-?>
